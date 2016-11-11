7a42ecf0cc4a8993e24296235c4c0b0c5ceea926
Ismael Nascimento <ismaeln.business@gmail.com>
2016-11-11 21:08:19 -0200

    Update SendDataToBluetoothSerial.ino

--- SendDataToBluetoothSerial.ino
+++ SendDataToBluetoothSerial.ino
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2016 Arduino da Peste.  All rights reserved.
+ */
+
 #include <CurieBLE.h>
 
 BLEPeripheral blePeripheral;  // BLE Peripheral Device (the board you're programming)
@@ -7,6 +11,9 @@ BLEService ledService("19B10000-E8F2-537E-4F6C-D104768A1214"); // BLE LED Servic
 BLEUnsignedCharCharacteristic switchCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
 
 const int ledPin = 13; // pin to use for the LED
+String recebido[3];
+int v = 0;
+int iNum1, iNum2, iNum3;
 
 void setup() {
   Serial.begin(9600);
@@ -15,7 +22,7 @@ void setup() {
   pinMode(ledPin, OUTPUT);
 
   // set advertised local name and service UUID:
-  blePeripheral.setLocalName("LED");
+  blePeripheral.setLocalName("Sirene");
   blePeripheral.setAdvertisedServiceUuid(ledService.uuid());
 
   // add service and characteristic:
@@ -45,19 +52,66 @@ void loop() {
     while (central.connected()) {
       // if the remote device wrote to the characteristic,
       // use the value to control the LED:
-      if (switchCharacteristic.written()) {
-        if (switchCharacteristic.value()) {   // any value other than 0
-          Serial.println("LED on");
-          digitalWrite(ledPin, HIGH);         // will turn the LED on
-        } else {                              // a 0 value
-          Serial.println(F("LED off"));
-          digitalWrite(ledPin, LOW);          // will turn the LED off
+        if (switchCharacteristic.written()) {
+          recebido[v] = switchCharacteristic.value();
+    
+          iNum1 = recebido[0].toInt();
+          iNum2 = recebido[1].toInt();
+          iNum3 = recebido[2].toInt();
+          
+          // Verificação de impressão em serial monitor
+          if(recebido[2] != ""){
+            Serial.println("Number1 + 1 = ");
+             Serial.println(iNum1 + 1);
+            Serial.println("Number2 + 1 = ");
+            Serial.println(iNum2 + 1);
+            Serial.println("Number3 + 1 = ");
+            Serial.println(iNum3 + 1);
+          }
+          
+          // Lê mais um valor do bluetooth
+          v++;
+    
+          
+          /*
+          if (switchCharacteristic.written()) {
+            if (switchCharacteristic.value() == 1) {   // any value other than 0
+              Serial.println("LED on");
+              digitalWrite(ledPin, HIGH);         // will turn the LED on
+            } else {                              // a 0 value
+              Serial.println(F("LED off"));
+              digitalWrite(ledPin, LOW);          // will turn the LED off
+            }
+            */
         }
+        
       }
+      // when the central disconnects, print it out:
+        Serial.print(F("Disconnected from central: "));
+        Serial.println(central.address());
     }
+}
+    
 
-    // when the central disconnects, print it out:
-    Serial.print(F("Disconnected from central: "));
-    Serial.println(central.address());
+String leStringSerial(){
+  String conteudo = "";
+  char caractere;
+  
+  // Enquanto receber algo pela serial
+  while(Serial.available() > 0) {
+    // Lê byte da serial
+    caractere = Serial.read();
+    // Ignora caractere de quebra de linha
+    if (caractere != '\n'){
+      // Concatena valores
+      conteudo.concat(caractere);
+    }
+    // Aguarda buffer serial ler próximo caractere
+    delay(10);
   }
+    
+  Serial.print("Recebi: ");
+  Serial.println(conteudo);
+    
+  return conteudo;
 }
